b479ce0dc0166fd088ea816188ec69cb400463b6
Thomas Harning Jr <harningt@gmail.com>
2019-05-20 20:51:15 -0400

    copyright: update copyright for latest changed file with one

--- src/main/groovy/us/eharning/gradle/defaults/DefaultsPlugin.groovy
+++ src/main/groovy/us/eharning/gradle/defaults/DefaultsPlugin.groovy
@@ -1,6 +1,6 @@
 /*
  * Copyright 2014-2016 Andrew Oberstar
- * Copyright 2017 Thomas Harning Jr. <harningt@gmail.com>
+ * Copyright 2017, 2019 Thomas Harning Jr. <harningt@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
